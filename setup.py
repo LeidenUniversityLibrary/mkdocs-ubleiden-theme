a230a433287cfd8da8212560b587d52b40552cf7
Ben Companjen <ben@companjen.name>
2022-12-13 14:56:46 +0100

    Set minimal Python to 3.7

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         'Topic :: Software Development :: Documentation',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
